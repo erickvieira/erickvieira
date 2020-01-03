4315afb09af8ed6f469acb8d531e05e12d524e47
erickvieira <erick-vieira-s@outlook.com>
2020-01-03 17:29:18 -0300

    inclusão da tag para códigos

--- src/components/code-syntax/code-syntax.jsx
+++ src/components/code-syntax/code-syntax.jsx
@@ -3,15 +3,28 @@ import PropTypes from 'prop-types'
 
 const CodeSyntax = ({ children, lang }) => (
   <>
-    <span className={ `code-syntax ${ lang || '' }` }>
+    <span className={ `code-syntax ${lang}` }>
       { children }
     </span>
   </>
 )
 
-Layout.propTypes = {
+CodeSyntax.propTypes = {
   children: PropTypes.node.isRequired,
   lang: PropTypes.string
 }
 
 export default CodeSyntax
+
+export const Code = ({ children, c }) => (
+  <>
+    <span className={ `code ${c}` }>
+      { children }
+    </span>
+  </>
+)
+
+Code.propTypes = {
+  children: PropTypes.node.isRequired,
+  c: PropTypes.string.isRequired
+}
